676c7426a13e00686d88a9c7e3f66b93b9482487
CM77 <29871766+CM77@users.noreply.github.com>
2022-10-07 17:37:24 +0200

    Create dungeon part via KeyEvent

--- src/main/java/text_dungeon/TextDungeonController.java
+++ src/main/java/text_dungeon/TextDungeonController.java
@@ -5,12 +5,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.css.PseudoClass;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 
 public class TextDungeonController implements Initializable {
 
@@ -28,16 +30,32 @@ public class TextDungeonController implements Initializable {
 	@FXML
 	private TextArea mainTa;
 
+	DungeonCreator dc = new DungeonCreator();
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		DungeonCreator dc = new DungeonCreator();
 		mainTa.setEditable(false);
-		mainTa.setText(dc.createDungeon());
+//		mainTa.setText(dc.createDungeon());
 		mainTa.pseudoClassStateChanged(PseudoClass.getPseudoClass("centered"), true);
+		starteTastenEventHandler();
 	}
 
 	public Parent getRoot() {
 		return root;
 	}
 
+	private void starteTastenEventHandler() {
+		root.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				switch (event.getCode()) {
+					case UP:
+						// TODO todo
+						mainTa.setText(dc.createDungeon());
+					default:
+						break;
+				}
+			}
+		});
+	}
 }
